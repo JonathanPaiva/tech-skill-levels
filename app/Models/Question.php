c02d32c4ef754ad5a3c4925208c0b2d2d8ff9955
JonathanPaiva <jonathan.paiva.m@hotmail.com>
2023-11-23 11:57:42 -0300

    Atualizado Model Question

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -11,8 +11,34 @@ class Question extends Model
     use HasFactory;
     
     protected $fillable = [
-        'question'
+        'question',
+        'theme_id',
+        'level_id'
     ];
+
+    public static function createRegister($questionRequest):void {
+
+        Question::create($questionRequest);
+    }
+
+    public static function getById($id) : Question {
+
+        return Question::findOrFail($id);
+    }
+
+    public static function editRegister($questionRequest) : void {
+
+        $question = self::getById($questionRequest->id);
+
+        $question->update($questionRequest->all());
+    }
+
+    public static function deleteRegister($id) : void {
+
+        $question = self::getById($id);
+
+        $question->delete();
+    }
 
     public function theme()
     {
